97ac330ccf6a0dc9db784cfe737409ed2b9d7965
Alexandria McPherson <alexandrialeemcpherson@gmail.com>
2023-06-24 21:57:24 +0900

    Set up server and llm

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-import 'dotenv/config';
-import { OpenAI } from "langchain/llms/openai";
+// Use environment variables
+require('dotenv').config();
+
+// Import express and create instance
+const express = require('express');
+const app = express();
+
+// Import LLM functions
+const { sendPrompt } = require('./llm');
+
+// Enable JSON
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Get port from .env
+const PORT = process.env.PORT;
+
+// Home route
+app.get('/', (request, response) => {
+    response.send('hello world');
+});
+
+// Submit prompt
+app.post('/', (request, response) => {
+    // Get prompt from req and send to OpenAI
+    if (request.body.prompt) {
+        let llmResponse = sendPrompt(request.body.prompt);
+        response.send(llmResponse)
+    } else {
+        console.error('No prompt provided');
+    }
+})
+
+// Activate server
+app.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT)
+});
---
+++ llm.js
@@ -0,0 +1,15 @@
+// Use environment variables
+require('dotenv').config();
+
+// Import OpenAI from langchain
+const { OpenAI } = require("langchain/llms/openai");
+
+// Create LLM instance
+const llm = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9});
+
+async function sendPrompt(prompt) {
+    const result = await llm.call(prompt);
+    return result
+}
+
+module.exports = { sendPrompt }
